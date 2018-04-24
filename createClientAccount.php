933453e6f77a289b4094c3914de0237d9c15286c
John Cody Wilkinson <37634748+jcwilkin@users.noreply.github.com>
2018-04-23 23:14:47 -0500

    Edit Html

--- createClientAccount.php
+++ createClientAccount.php
@@ -1,3 +1,7 @@
+<?php
+include("DBH.php")
+?>
+
 <html>
 
 <head>
@@ -6,224 +10,252 @@
 </head>
 
 <body>
-  <div id = "frm">
-    <form action = "InsertClient.php" method="POST">
-      <p>
+  
+    <form action = "insertClient.php" method="POST">
         <label> Create Client Account </label>
-      </p>
-      <p>
+        <br>
         <label> Report Date: </label>
-        <input type="text" id = "RPT_Date" name = "date">
+        <input type="text" id = "RPT_Date" name = "RPT_Date">
+        <br>
 
         <label> RCD TRAN: </label>
-        <input type="text" id="RCD_TRAN" name ="tran"
-      </p>
-      <p>
-          <label> Organization Code: </label>
-          <input type="text" id="ORG_CODE" name="org"
+        <input type="text" id="RCD_TRAN" name ="RCD_TRAN">
+        <br>
+       
+        <label> Organization Code: </label>
+        <input type="text" id="ORG_CODE" name="ORG_CODE"/>
+        <br>
 
         <label> Location Code: </label>
-        <input type="text" id="LOC_CODE" name = "loc"
-      </p>
-      <p>
+        <input type="text" id="LOC_CODE" name = "LOC_CODE"/>
+        <br>
+      
+      
         <label> Client ID: </label>
-        <input type="text" id = "CLI_ORG_ID" name="client_org"
+        <input type="text" id = "CLI_ORG_ID" name="CLI_ORG_ID"/>
+        <br>
 
         <label> Client Status: </label>
-        <input type="text" id="CLI_STATUS" name="client_stat">
-      </p>
-      <p>
+        <input type="text" id="CLI_STATUS" name="CLI_STATUS">
+        <br>
+      
+      
         <label> Admission Date: </label>
-        <input type="text" id="ADM_DATE" name ="admit"
+        <input type="text" id="ADM_DATE" name ="ADM_DATE"/>
+        <br>
 
         <label> ADM_REFFER: </label>
-        <input type="text" id="ADM_REFFER" name=""="admit_ref">
+        <input type="text" id="ADM_REFFER" name=""="ADM_REFFER">
+        <br>
 
         <label> ADM_REFFER_OR: </label>
-        <input type="text" id="ADM_REFFER_OR" name="admit_ref_or">
-      </p>
-      <p>
+        <input type="text" id="ADM_REFFER_OR" name="ADM_REFFER_OR">
+        <br>
+      
+      
         <label> Legal Status: </label>
-        <input type="text" id="LEG_STATUS" name=""="legal">
-      </p>
-      <p>
+        <input type="text" id="LEG_STATUS" name=""="LEG_STATUS">
+        <br>
+      
+      
         <label> Last Name: </label>
-        <input type="text" id="LAST_NAME" name="last" />
+        <input type="text" id="LAST_NAME" name="LAST_NAME" />
+        <br>
 
         <label> First Name: </label>
-        <input type="text" id="FIRST_NAME" name="first" />
+        <input type="text" id="FIRST_NAME" name="FIRST_NAME" />
+        <br>
 
         <label> Maiden Name: <label>
-        <input type="text" id = "MAID_NAME" name="maid"
-      </p>
-      <p>
+        <input type="text" id = "MAID_NAME" name="MAID_NAME"/>
+        <br>
+      
+      
         <label> Social Security #: </label>
-        <input type="text" id="SS_NUMBER" name="ss_num"
-      <p>
+        <input type="text" id="SS_NUMBER" name="SS_NUMBER"/>
+        <br>
+      
        <label> Birth Date: </label>
-       <input type="text" id ="BIRTH_DATE" name ="birth"
+       <input type="text" id ="BIRTH_DATE" name ="BIRTH_DATE"/>
+       <br>
 
         <label> Age: </label>
-        <input type="text" id="AGE" name="age">
-      </p>
-      <p>
+        <input type="text" id="AGE" name="AGE">
+        <br>
+      
+      
         <label> Sex: </label>
-        <input type="text" id="SEX" name="sex">
+        <input type="text" id="SEX" name="SEX">
+        <br>
 
         <label> Race: </label>
-        <input type="text" id="RACE" name="race">
+        <input type="text" id="RACE" name="RACE">
+        <br>
 
         <label> Hispanic: </label>
-        <input type="text" id="HISPANIC" name="hispanic">
-      </p>
-      <p>
+        <input type="text" id="HISPANIC" name="HISPANIC">
+        <br>
+      
+      
         <label> Education: </label>
-        <input type="text" id="EDUCATION" name="edu">
-      </p>
-      <p>
+        <input type="text" id="EDUCATION" name="EDUCATION">
+        <br>
+      
+      
         <label> Marital: </label>
-        <input type="text" id="MARITAL" name="marital">
-      </p>
-      <p>
+        <input type="text" id="MARITAL" name="MARITAL">
+        <br>
+      
+      
         <label> ADM_CITY: </label>
-        <input type="text" id="ADM_CITY" name="admin_city">
-      </p>
-      <p>
-        <label> Living Arrangments: </label>
-        <input type="text" id="LIVING_ARR" name="living">
-      </p>
-      <p>
-        <label> Residential Arrangments: </label>
-        <input type="text" id="RESID_ARR" name="resident">
-      </p>
-      <p>
+        <input type="text" id="ADM_CITY" name="ADM_CITY">
+        <br>
+      
+      
+      
         <label> EMPLOYMENT: </label>
-        <input type="text" id="EMPLOYMENT" name="employ">
-      </p>
-      <p>
+        <input type="text" id="EMPLOYMENT" name="EMPLOYMENT">
+        <br>
+      
+      
         <label> Income Source: </label>
-        <input type="text" id="INCOME_SRC" name="income_src">
-      </p>
-      <p>
+        <input type="text" id="INCOME_SRC" name="INCOME_SRC">
+        <br>
+      
+      
         <label> Income Housing: </label>
-        <input type="text" id="INCOME_HOU" name="income_hou">
-      </p>
-      <p>
+        <input type="text" id="INCOME_HOU" name="INCOME_HOU">
+        <br>
+      
+      
         <label> INCOME_DEP: </label>
-        <input type="text" id="INCOME_DEP" name="income_dep">
-      </p>
-      <p>
+        <input type="text" id="INCOME_DEP" name="INCOME_DEP">
+        <br>
+      
+      
         <label> SSI: </label>
-        <input type="text" id="ELIG_SSI" name="SSI">
+        <input type="text" id="ELIG_SSI" name="ELIG_SSI">
+        <br>
 
         <label>  Medicaid: </label>
-        <input type="text" id="ELIG_MCAID" name="mcaid">
-      </p>
-      <p>
+        <input type="text" id="ELIG_MCAID" name="ELIG_MCAID">
+        <br>
+      
         <label> Payment: </label>
-        <input type="text" id="PAYMENT" name="payment">
-      </p>
-      <p>
+        <input type="text" id="PAYMENT" name="PAYMENT">
+        <br>
+    
         <label> Vetern Status:  </label>
-        <input type="text" id="VET_STATUS" name="vet">
-      </p>
-      <p>
+        <input type="text" id="VET_STATUS" name="VET_STATUS">
+        <br>
+      
         <label> Handicap 1: </label>
-        <input type="text" id="HANDICAP_1" name="hand_1">
+        <input type="text" id="HANDICAP_1" name="HANDICAP_1">
+        <br>
 
         <label> Handicap 2: </label>
-        <input type="text" id="HANDICAP_2" name="hand_2">
-      </p>
-      <p>
+        <input type="text" id="HANDICAP_2" name="HANDICAP_2">
+        <br>
+      
         <label> Problem 1: </label>
-        <input type="text" id="PROBLEM_1" name="prob_1">
+        <input type="text" id="PROBLEM_1" name="PROBLEM_1">
+        <br>
 
         <label> Problem 2: </label>
-        <input type="text" id="PROBLEM_2" name="prob_2">
-      </p>
-      <p>
+        <input type="text" id="PROBLEM_2" name="PROBLEM_2">
+        <br>
+      
         <label> DISAB_CATE: </label>
-        <input type="text" id="DISAB_CATE" name="cate">
+        <input type="text" id="DISAB_CATE" name="DISAB_CATE">
+        <br>
 
         <label> DISAB_DUAL: </label>
-        <input type="text" id="DISAB_DUAL" name="dual">
-      </p>
-      <p>
+        <input type="text" id="DISAB_DUAL" name="DISAB_DUAL">
+        <br>
+      
         <label> SPMI: </label>
-        <input type="text" id="SPMI" name="spmi">
+        <input type="text" id="SPMI" name="SPMI">
+        <br>
 
         <label> SEDC: </label>
-        <input type="text" id="SEDC" name="sedc">
-      </p>
-      <p>
+        <input type="text" id="SEDC" name="SEDC">
+        <br>
+      
         <label> Diagnosis 1:  </label>
-        <input type="text" id="DIAGNOSIS1" name="diag_1">
+        <input type="text" id="DIAGNOSIS1" name="DIAGNOSIS1">
+        <br>
 
         <label> Diagnosis 2: </label>
-        <input type="text" id="DIAGNOSIS2" name="diag_2">
-      </p>
-      <p>
+        <input type="text" id="DIAGNOSIS2" name="DIAGNOSIS2">
+        <br>
+      
         <label> DCS ID: </label>
-        <input type="text" id="DCS_ID" name="dcs">
-      </p>
-      <p>
+        <input type="text" id="DCS_ID" name="DCS_ID">
+        <br>
+      
         <label> INAPT SERV: </label>
-        <input type="text" id="INAPT_SERV" name="inapt">
+        <input type="text" id="INAPT_SERV" name="INAPT_SERV">
+        <br>
 
         <label> RESID SERV: </label>
-        <input type="text" id="RESID_SERV" name="resid">
-      </p>
-      <p>
+        <input type="text" id="RESID_SERV" name="RESID_SERV">
+        <br>
+    
         <label> PARTI SERV: </label>
-        <input type="text" id="PARTI_SERV" name="parti">
+        <input type="text" id="PARTI_SERV" name="PARTI_SERV">
+        <br>
 
         <label> OUTPA SERV: </label>
-        <input type="text" id="OUTPA_SERV" name="outpa">
-      </p>
-      <p>
+        <input type="text" id="OUTPA_SERV" name="OUTPA_SERV">
+        <br>
+      
         <label> CASEM SERV:  </label>
-        <input type="text" id="CASEM_SERV " name="casem">
-      </p>
-      <p>
+        <input type="text" id="CASEM_SERV" name="CASEM_SERV">
+        <br>
+      
         <label> DIS DATE: </label>
-        <input type="text" id="DIS_DATE" name="dis_date">
+        <input type="text" id="DIS_DATE" name="DIS_DATE">
+        <br>
 
         <label> DIS STATUS: </label>
-        <input type="text" id="DIS_STATUS" name="dis_status">
-      </p>
-      <p>
+        <input type="text" id="DIS_STATUS" name="DIS_STATUS">
+        <br>
+      
         <label> DIS REFFER: </label>
-        <input type="text" id="DIS_REFFER" name="dis_ref">
+        <input type="text" id="DIS_REFFER" name="DIS_REFFER">
+        <br>
 
         <label> DIS REFFER OR: </label>
-        <input type="text" id="DIS_REFFER_OR" name="dis_ref_or">
-      </p>
-      <p>
+        <input type="text" id="DIS_REFFER_OR" name="DIS_REFFER_OR">
+        <br>
+      
         <label> Medicaid #: </label>
-        <input type="text" id="MCAID_NUMBER" name="mcaid_num">
-      </p>
-      <p>
+        <input type="text" id="MCAID_NUMBER" name="MCAID_NUMBER">
+        <br>
+      
         <label> State ID: </label>
-        <input type="text" id="DIS_CITY" name="state">
-      </p>
-      <p>
+        <input type="text" id="DIS_CITY" name="DIS_CITY">
+        <br>
+      
         <label> INTGR TREAT: </label>
-        <input type="text" id="INTGR_TREAT" name="intgr">
+        <input type="text" id="INTGR_TREAT" name="INTGR_TREAT">
+        <br>
 
         <label> ACT TREAT: </label>
-        <input type="text" id="ACT_TREAT" name="act">
-      </p>
-      <p>
+        <input type="text" id="ACT_TREAT" name="ACT_TREAT">
+        <br>
+      
         <label> Program Code: </label>
-        <input type="text" id="PROGRAM_CODE" name="prgm_code">
-      </p>
-      <p>
+        <input type="text" id="PROGRAM_CODE" name="PROGRAM_CODE">
+        <br>
+      
         <label> ENT DATE: </label>
-        <input type="text" id="ENT_DATE" name="ent">
+        <input type="text" id="ENT_DATE" name="ENT_DATE">
+        <br>
 
         <label> EXT DATE: </label>
-        <input type="text" id="EXT_DATE" name="ext">
-      </p>
+        <input type="text" id="EXT_DATE" name="EXT_DATE">
+        <br>
 
 
         <input type="submit" id="btn" name="Submit" />
